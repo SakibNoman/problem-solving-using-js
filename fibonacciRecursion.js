f77bbfc68ff5a2eb0af7f35d6aa4b1c3a1b764ba
Sakib Noman <skbnmn@gmail.com>
2021-01-19 22:50:56 +0600

    swap using js

--- fibonacciRecursion.js
+++ fibonacciRecursion.js
@@ -1,12 +1,13 @@
-var fiboArr = [0, 1];
-var currentPosition = 2;
 function fibo(num) {
-    fiboArr[currentPosition] = fiboArr[currentPosition - 1] + fiboArr[currentPosition - 2];
-    currentPosition++;
-    if (currentPosition == num+1) {
-        return fiboArr;
+    if(num==0){
+        return 0;
     }
-    return fibo(num);
+    else if(num==1){
+        return 1;
+    }
+    else{
+        return fibo(num-1)+fibo(num-2);
+    }
 }
 
 var result = fibo(15);
