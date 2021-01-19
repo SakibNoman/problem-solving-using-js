d7ad26c11b2bd2cf51f9c0f5dd0e7f53fc347c12
Sakib Noman <skbnmn@gmail.com>
2021-01-19 23:21:22 +0600

    random number for dice

---
+++ jsSpecialSwap.js
@@ -0,0 +1,7 @@
+var numberOne = 5;
+var numberTwo = 11;
+console.log("before swap: number one = "+numberOne+" number two = "+numberTwo);
+
+[numberOne,numberTwo]=[numberTwo,numberOne];
+
+console.log("after swap: number one = "+numberOne+" number two = "+numberTwo)
---
+++ randomNumber.js
@@ -0,0 +1,8 @@
+// random number for a dice , 1-6
+
+console.log(Math.ceil((Math.random()*6)));
+
+
+// ceil is for prevent coming 0
+
+// multiplied by 6 is for coming number upto 6
